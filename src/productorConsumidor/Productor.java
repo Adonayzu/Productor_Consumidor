49bddb5af0504384bae728e7579aa4c4a8e943e1
adonay <strlemuszu@upana.edu.gt>
2024-08-30 22:21:34 -0600

    Commit final de la tarea Productor Consumidor

--- src/productorConsumidor/Productor.java
+++ src/productorConsumidor/Productor.java
@@ -4,42 +4,40 @@ import java.util.Queue;
 import java.util.Random;
 
 class Productor extends Thread {
-    private Queue<Integer> cola; //Queue<Integer> --> almacena los numeros enteros
-    private int capacidad; //capacidad maxima
+    private Queue<Integer> cola;
+    private int capacidad;
+    private int ciclos;  // Número de ciclos de producción
 
-    // Constructor para el productor
-    public Productor(Queue<Integer> cola, int capacidad) {
+    //constructor
+    public Productor(Queue<Integer> cola, int capacidad, int ciclos) {
         this.cola = cola;
         this.capacidad = capacidad;
+        this.ciclos = ciclos;
     }
 
     @Override
     public void run() {
         Random aleatorio = new Random();
-        try{
-            for (int i =0; i < 1000; i++){
-                synchronized (cola) { // ayuda para que solo un hilo se ejecute a la vez
-                    while (cola.size() == capacidad){
-                        cola.wait();
+        try {
+            for (int ciclo = 0; ciclo < ciclos; ciclo++) {
+                synchronized (cola) {
+                    while (cola.size() < capacidad) {
+                        int valor = aleatorio.nextInt(100) + 1;
+                        cola.add(valor);
+                        System.out.println("El productor a producio el número: " + valor);
                     }
-                    int valor = aleatorio.nextInt(100)+ 1; //genera un numero aleatorio de 0 a 99 y se le suma 1 para que sea el numero correcto del 1 al 100
-                    cola.add(valor); // se agrega el valor a la cola
-                    System.out.println("El productor a producido: " + valor);
-
-
-                    if(cola.size() == capacidad){ // si es igual a la cola que es 10 que notifique al consumidorq que esta lleno
-                        cola.notifyAll();
-                    }
-
+                    cola.notifyAll();
+                    cola.wait();
                 }
             }
-        }catch (InterruptedException e){
+
+            // para detener el  while del consumidor
+            synchronized (cola) {
+                cola.add(-1); // se agrega a la cola
+                cola.notifyAll();
+            }
+        } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-
-
     }
 }
-
-
-
--- src/productorConsumidor/ProductorConsumidor.java
+++ src/productorConsumidor/ProductorConsumidor.java
@@ -5,32 +5,63 @@ import java.util.Queue;
 
 public class ProductorConsumidor {
     public static void main(String[] args) {
-        Queue<Integer>  cola = new LinkedList<>(); // LinkedList--> Se puede añadir o eliminar elementos sin preocuparse por el tamaño inicial
-        int capacidad = 10; // capacidad de la cola
-        int [] conteoDecenas = new int [10]; // sirve para contar numeros de diferentes rangos en decenas
+        Queue<Integer> cola = new LinkedList<>();  // LinkedList--> Se puede añadir o eliminar elementos sin preocuparse por el tamaño inicial
+        int capacidad = 10;
+        int ciclos = 100;
+        int[] conteoDecenas = new int[10];
 
-        Productor productor = new Productor(cola, capacidad);
-        Consumidor consumidor = new Consumidor(cola, conteoDecenas,capacidad);
+        Productor productor = new Productor(cola, capacidad, ciclos);
+        Consumidor consumidor = new Consumidor(cola, conteoDecenas, capacidad);
 
         productor.start();
-
         consumidor.start();
 
         try{
-            productor.join(); //Espera a que el productor termine su ejecucion
-            synchronized (cola){    // solo un hilo se pueda ejecutar en este bloque d codigo
-                cola.add(-1); // sirve como ua señal para el consumidor deba detenerse y el -1 es par que cuando consuma y llegue a ese valor se detenga
-                cola.notifyAll();
-            }
-            consumidor.join(); // Espera a que el hilo cosumidor termine de ejecutarse
-        }catch (InterruptedException e){
+            productor.join();
+            consumidor.join();
+        }catch(InterruptedExceptione){
             Thread.currentThread().interrupt();
+        }package productorConsumidor;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+        public class ProductorConsumidor {
+            public static void main(String[] args) {
+                Queue<Integer> cola = new LinkedList<>();  // LinkedList--> Se puede añadir o eliminar elementos sin preocuparse por el tamaño inicial
+                int capacidad = 10;
+                int ciclos = 100;
+                int[] conteoDecenas = new int[10];
+
+                Productor productor = new Productor(cola, capacidad, ciclos);
+                Consumidor consumidor = new Consumidor(cola, conteoDecenas, capacidad);
+
+
+                productor.start();
+                consumidor.start();
+
+                try{
+                    productor.join();
+                    consumidor.join();
+                }catch(InterruptedException e){
+                    Thread.currentThread().interrupt();
+                }
+
+                // Mostrar el resultado final
+                for (int i = 0; i < 10; i++) {
+                    int rangoInferior = i * 10 + 1;
+                    int rangoSuperior = (i + 1) * 10;
+                    System.out.println("La cantidad de números entre " + rangoInferior + " y " + rangoSuperior + ": " + conteoDecenas[i]);
+                }
+            }
         }
-        //Para mostrar el resultado final
-        for (int i = 0; i < 10; i++){
-            int rangoInferior = i *10 +1;
-            int rangoSuperior = (i + 1) *10;
-            System.out.println("La Cantidad de numeros entre " + rangoInferior + " y " + rangoSuperior + ": " + conteoDecenas[i]);
+
+
+        // Mostrar el resultado final
+        for (int i = 0; i < 10; i++) {
+            int rangoInferior = i * 10 + 1;
+            int rangoSuperior = (i + 1) * 10;
+            System.out.println("La cantidad de números entre " + rangoInferior + " y " + rangoSuperior + ": " + conteoDecenas[i]);
         }
     }
 }
